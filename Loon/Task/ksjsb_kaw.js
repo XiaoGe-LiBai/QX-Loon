735fa865549c60e6c53c6d08539acaf42f8669ce
XiaoGe-LiBai <60430782+XiaoGe-LiBai@users.noreply.github.com>
2025-11-06 14:14:05 +0800

    Update ksjsb_kaw.js

--- Loon/Task/ksjsb_kaw.js
+++ Loon/Task/ksjsb_kaw.js
@@ -28,7 +28,7 @@
       $notification.post(
         '快手极速版 kaw 参数',
         'kaw 参数捕获成功',
-        `长度: ${kawValue.length} 字符`,
+        `快手极速版kaw: ${kawValue}`,
         {
           clipboard: `快手极速版kaw: ${kawValue}`
         }
